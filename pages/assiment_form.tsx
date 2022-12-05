8b83b79fa3833132459783cb8c7340935ef32b79
Potae99 <101413178+Potae99@users.noreply.github.com>
2022-12-05 16:42:36 +0700

    Add files via upload

---
+++ pages/assiment_form.tsx
@@ -0,0 +1,183 @@
+export default function Homepage(){
+
+   const  professor =[
+      { 'ลำดับ':1,
+         'หัวข้อ':'อาจาร์ยได้แจ้งกำหนดการเรียนในส่วนที่อาจาร์ยรับผิดชอบ'
+      },
+      {'ลำดับ':2,
+      'หัวข้อ':'อาจาร์ยได้แจ้งกำหนดการเรียนในส่วนที่อาจาร์ยรับผิดชอบ'
+      },
+      {'ลำดับ':3,
+      'หัวข้อ':'อาจาร์ยได้แจ้งกำหนดการเรียนในส่วนที่อาจาร์ยรับผิดชอบ'
+      },
+      {'ลำดับ':4,
+      'หัวข้อ':'อาจาร์ยได้แจ้งกำหนดการเรียนในส่วนที่อาจาร์ยรับผิดชอบ'
+      },
+      {'ลำดับ':5,
+      'หัวข้อ':'อาจาร์ยได้แจ้งกำหนดการเรียนในส่วนที่อาจาร์ยรับผิดชอบ'
+      },
+      {'ลำดับ':6,
+      'หัวข้อ':'อาจาร์ยได้แจ้งกำหนดการเรียนในส่วนที่อาจาร์ยรับผิดชอบ'
+      },
+      {'ลำดับ':7,
+      'หัวข้อ':'อาจาร์ยได้แจ้งกำหนดการเรียนในส่วนที่อาจาร์ยรับผิดชอบ'
+      },
+      {'ลำดับ':8,
+      'หัวข้อ':'อาจาร์ยได้แจ้งกำหนดการเรียนในส่วนที่อาจาร์ยรับผิดชอบ'
+      },
+      {'ลำดับ':9,
+      'หัวข้อ':'อาจาร์ยได้แจ้งกำหนดการเรียนในส่วนที่อาจาร์ยรับผิดชอบ'
+      },
+      {'ลำดับ':10,
+      'หัวข้อ':'อาจาร์ยได้แจ้งกำหนดการเรียนในส่วนที่อาจาร์ยรับผิดชอบ'
+      },
+      {'ลำดับ':11,
+      'หัวข้อ':'อาจาร์ยได้แจ้งกำหนดการเรียนในส่วนที่อาจาร์ยรับผิดชอบ'
+      },
+      {'ลำดับ':12,
+      'หัวข้อ':'อาจาร์ยได้แจ้งกำหนดการเรียนในส่วนที่อาจาร์ยรับผิดชอบ'
+      },
+      {'ลำดับ':13,
+      'หัวข้อ':'อาจาร์ยได้แจ้งกำหนดการเรียนในส่วนที่อาจาร์ยรับผิดชอบ'
+      },
+      {'ลำดับ':14,
+      'หัวข้อ':'อาจาร์ยได้แจ้งกำหนดการเรียนในส่วนที่อาจาร์ยรับผิดชอบ'
+      },
+      {'ลำดับ':15,
+      'หัวข้อ':'อาจาร์ยได้แจ้งกำหนดการเรียนในส่วนที่อาจาร์ยรับผิดชอบ'
+      },
+      {'ลำดับ':16,
+      'หัวข้อ':'อาจาร์ยได้แจ้งกำหนดการเรียนในส่วนที่อาจาร์ยรับผิดชอบ'
+      },
+      {'ลำดับ':17,
+      'หัวข้อ':'อาจาร์ยได้แจ้งกำหนดการเรียนในส่วนที่อาจาร์ยรับผิดชอบ'
+      },
+      {'ลำดับ':18,
+      'หัวข้อ':'อาจาร์ยได้แจ้งกำหนดการเรียนในส่วนที่อาจาร์ยรับผิดชอบ'
+      },
+      {'ลำดับ':19,
+      'หัวข้อ':'อาจาร์ยได้แจ้งกำหนดการเรียนในส่วนที่อาจาร์ยรับผิดชอบ'
+      },
+      {'ลำดับ':20,
+      'หัวข้อ':'อาจาร์ยได้แจ้งกำหนดการเรียนในส่วนที่อาจาร์ยรับผิดชอบ'
+      }
+   ]
+   
+  
+
+    
+
+
+  
+    return ( <>
+    <div className="flex-col space-y-3 bg-gray-300 block  h-full justify-center p-4">
+        <div className=" text-center ">
+         <h1>แบบประเมินการสอนของอาจาร์ยโดยนิสิต</h1>
+         <h2>หลักสูตรประกาศนียบัตรผู้ช่วยพยาบาล</h2>
+         <h3>คณะพยาบาลศาสตร์ มหาวิทยาลัยนเรศวร</h3>
+         <h4>การสอนแบบเน้นการปฎิบัติ</h4>
+        </div>
+        <div>
+            <h5>คำชี้แจง</h5>
+            <div className=" flex">
+                1.ทำเครื่องหมาย
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+                </svg>
+                ลงใน
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M5.25 7.5A2.25 2.25 0 017.5 5.25h9a2.25 2.25 0 012.25 2.25v9a2.25 2.25 0 01-2.25 2.25h-9a2.25 2.25 0 01-2.25-2.25v-9z" />
+                </svg>
+                หรือเติมข้อความลงในช่องว่างตามความเป็นจริง
+            </div>
+            <div>
+                2.เป็นการประเมินภาพรวมของอาจาร์ยทุกท่านที่เข้าร่วมการสอน
+            </div>
+            <div>
+                3.ถ้าท่านเข้าเรียนไม่สม่ำเสมอ &#40;ไม่ถึง 80&#37; ของเวลาเรียนทั้งหมด&#41; โปรดสละสิทธิ์ในการตอบ
+            </div>
+            <h6>ส่วนที่ 1 ข้อมูลพื้นฐาน</h6>
+            <form>
+             <div>
+                <p>1.รหัสรายวิชาที่เรียน</p>
+                <input className="border border-gray-300 outline-none placeholder-gray-400 pl-9 pr-4 py-1 rounded-md transition focus:ring-2 focus:ring-green-300"  type="text" id="name" autoComplete="name" required />
+             </div>
+             <div>
+                <p>ชื่อรายวิชา</p>
+                <input className="border border-gray-300 outline-none placeholder-gray-400 pl-9 pr-4 py-1 rounded-md transition focus:ring-2 focus:ring-green-300"  type="text" id="name" autoComplete="name" required />
+             </div>
+             <div>
+                <p>หมู่เรียนที่ปฎิบัติการ</p>
+                <input className="border border-gray-300 outline-none placeholder-gray-400 pl-9 pr-4 py-1 rounded-md transition focus:ring-2 focus:ring-green-300"  type="text" id="name" autoComplete="name" required />
+             </div>
+             <div>
+                <p>2.ชื่ออาจาร์ยผู้สอน</p>
+                <input className="border border-gray-300 outline-none placeholder-gray-400 pl-9 pr-4 py-1 rounded-md transition focus:ring-2 focus:ring-green-300"  type="text" id="name" autoComplete="name" required />
+             </div>
+             <div>
+                <p>3.ภาคเรียน</p>
+                <input className="border border-gray-300 outline-none placeholder-gray-400 pl-9 pr-4 py-1 rounded-md transition focus:ring-2 focus:ring-green-300"  type="text" id="name" autoComplete="name" required />
+             </div>
+             <div className=" flex space-x-3 ">
+                <p>4.ท่านได้รับการประมวลรายวิชา</p>
+                <label>
+                <input type="checkbox" className="radio" value="1" name="fooby[1][]" />ใช่
+                </label>
+                <label>
+                <input type="checkbox" className="radio" value="1" name="fooby[1][]" />ไม่ใช่
+                </label>
+             </div>
+             <div className=" flex space-x-3 ">
+                <p>5.ท่านได้รับคู่มือการฝึกปฎิบัติ</p>
+                <label>
+                <input type="checkbox" className="radio" value="1" name="fooby[1][]" />ใช่
+                </label>
+                <label>
+                <input type="checkbox" className="radio" value="1" name="fooby[1][]" />ไม่ใช่
+                </label>
+             </div>
+             <p>ส่วนที่ 2 ข้อมูลเกี่ยวกับการสอนของอาจาร์ย</p>
+             {professor.map((professor, index) => (
+               <div data-index={index} className=" flex space-x-3">
+                  <p>{professor.ลำดับ}</p>
+                  <p>{professor.หัวข้อ}</p>
+                <label>
+                <input type="checkbox" className="radio" value="1" name="fooby[1][]" />น้อยที่สุด
+                </label>
+                <label>
+                <input type="checkbox" className="radio" value="1" name="fooby[1][]" />น้อย
+                </label>
+                <label>
+                <input type="checkbox" className="radio" value="1" name="fooby[1][]" />ปานกลาง
+                </label>
+                <label>
+                <input type="checkbox" className="radio" value="1" name="fooby[1][]" />มาก
+                </label>
+                <label>
+                <input type="checkbox" className="radio" value="1" name="fooby[1][]" />มากที่สุด
+                </label>
+               </div>
+
+
+             ))} 
+             <p>ส่วนที่ 3.ความคิดเห็นอื่นๆ</p>
+             <div>
+             <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">ความคิดเห็นเกี่ยวกับการสอนของอาจาร์ย และ/หรือ ปัญหาที่ต้องการให้มีการแก้ไชปรับปรุง</label>
+             <textarea id="message"  rows={4} className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="ใส่ข้อความที่นี่..."></textarea>
+
+
+             </div>
+
+
+            </form>
+
+        </div>
+
+       
+
+    </div>
+ 
+    </>
+    )
+  }
+  
